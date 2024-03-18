a3307ce6b43aa52c79a830b2a1c4add5a101fe9b
noriakis <nori@hgc.jp>
2024-03-18 09:45:07 +0900

    pathwayWithFactor

--- R/NMF.R
+++ R/NMF.R
@@ -77,7 +77,13 @@ alphaDiversityWithinSpecies <- function(stana, species, method="shannon", rank=5
 }
 
 #' plotAbundanceWithinSpecies
+#' 
+#' plot abundances using factor to sample matrix produced by NMF.
+#' 
 #' @param stana stana object
+#' @param species species ID
+#' @param tss perform total sum scaling
+#' @param return_data return only the data, not plot
 #' @export
 plotAbundanceWithinSpecies <- function(stana, species, tss=TRUE, return_data=FALSE) {
     if (is.null(stana@NMF[[species]])) {
@@ -102,4 +108,52 @@ plotAbundanceWithinSpecies <- function(stana, species, tss=TRUE, return_data=FAL
         ggplot(aes(x=group, y=value))+
         geom_boxplot()+
         facet_wrap(.~name)
+}
+
+
+#' pathwayWithFactor
+#' convert KO matrix per factor obtained by NMF function
+#' to pathway to factor matrix by summing the KOs in the pathway.
+#' @param stana stana boject
+#' @param species species ID
+#' @param tss perform total sum scaling to the resulting matrix
+#' @export
+pathwayWithFactor <- function(stana, species, tss=FALSE) {
+  dat <- stana@NMF[[species]]
+  dat <- basis(dat)
+
+  bfc <- BiocFileCache()
+  url <- bfcrpath(bfc,"https://rest.kegg.jp/link/ko/pathway")
+  url2 <- bfcrpath(bfc,"https://rest.kegg.jp/list/pathway")
+
+  summed <- data.frame(data.table::fread(url, header=FALSE))
+  namec <- data.frame(data.table::fread(url2, header=FALSE))
+  namec <- namec$V2 %>% setNames(namec$V1)
+  summed <- summed[grepl("ko", summed$V1),]
+  ## No global map
+  summed <- summed[!grepl("ko011", summed$V1),]
+  summed <- summed[!grepl("ko012", summed$V1),]
+
+  allpath <- unique(summed$V1)
+  pathdf <- do.call(rbind, lapply(allpath, function(i) {
+    tmp <- summed[summed$V1==i, ]
+    int <- length(intersect(row.names(dat), tmp$V2))
+    if (int>1) {
+      tmpsum <- apply(dat[intersect(row.names(dat), tmp$V2),], 2, sum)
+      return(c(i, tmpsum))
+    } else if (int==1) {
+      return(c(i, dat[intersect(row.names(dat), tmp$V2),]))
+    } else {
+      return(NULL)
+    }
+  })) %>% data.frame()
+  row.names(pathdf) <- pathdf[,1]
+  pathdf[,1] <- NULL
+  colnames(pathdf) <- as.character(paste0("factor",seq_len(ncol(pathdf))))
+  pathdf <- dplyr::mutate_all(pathdf, as.numeric)  
+  if (tss) {
+    pathdf <- apply(pathdf, 2, function(x) x / sum(x))
+  }
+  row.names(pathdf) <- namec[gsub("path:ko","map",row.names(pathdf))]
+  return(pathdf)
 }
